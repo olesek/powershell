2c9d6472eae12ccb701034b1029da3d7ae17fcc2
Joe Oleszczuk <joe@rattle.net>
2022-06-29 11:38:48 -0400

    Add winget

--- ipl-commands.ps1
+++ ipl-commands.ps1
@@ -5,38 +5,7 @@
 # Run "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned" as administrator and selcet "A"
 # *********************
 
-using namespace System.Management.Automation.Host
-
-function New-Menu {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory)]
-        [ValidateNotNullOrEmpty()]
-        [string]$Title,
-
-        [Parameter(Mandatory)]
-        [ValidateNotNullOrEmpty()]
-        [string]$Question
-    )
-    
-    $red = [ChoiceDescription]::new('&Red', 'Favorite color: Red')
-    $blue = [ChoiceDescription]::new('&Blue', 'Favorite color: Blue')
-    $yellow = [ChoiceDescription]::new('&Yellow', 'Favorite color: Yellow')
-
-    $options = [ChoiceDescription[]]($red, $blue, $yellow)
-
-    $result = $host.ui.PromptForChoice($Title, $Question, $options, 0)
-
-    switch ($result) {
-        0 { 'Your favorite color is Red' }
-        1 { 'Your favorite color is Blue' }
-        2 { 'Your favorite color is Yellow' }
-    }
-
-}
-
-
-
+# https://www.powershellgallery.com/
 
 # Clear Terminal Window
 Clear
@@ -54,6 +23,13 @@ Write-Host Time Zone set to:
 Get-TimeZone
 read-host Ready to begin Inital Program Load - Press ENTER to continue...
 
+# Install Winget
+Install-Module -Name WinGet
+
+# Install Winget Packages
+winget install firefox
+winget install Google Chrome
+
 # Update PC
 Install-Module -Name PSWindowsUpdate -Force
 Import-Module PSWindowsUpdate
@@ -61,6 +37,8 @@ Get-Command -Module PSWindowsUpdate
 Get-WindowsUpdate
 Get-WURebootStatus
 Install-WindowsUpdate -AcceptAll -AutoReboot
+
+
 
 # Install Google Chrome
 write-host "Installing Chrome.."
