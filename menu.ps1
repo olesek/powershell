2cd15fc19830d1a6780f500ffd346c1408eb992e
Joe Oleszczuk <joe@rattle.net>
2022-07-09 16:21:25 -0400

    update

--- menu.ps1
+++ menu.ps1
@@ -3,11 +3,15 @@ $a=Read-Host -Prompt "
 `n1. Show day of the week
 `n2. Show year
 `n3. Check for updates
+n4. Show PS Version
 `nPlease select an option"
 switch ($a){
     1{Get-Date -UFormat %A}
     2{Get-Date -UFormat %Y}
     3{Install-WindowsUpdate -AcceptAll -AutoReboot}
+    4{function Get-Version}
 }
-
+ {
+    $PSVersionTable.PSVersion
+}
 Write-Host $a
