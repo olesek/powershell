b3077e6dda8290defc258b95cbe2ec54f974f61d
Joe Oleszczuk <joe@rattle.net>
2022-07-09 16:12:12 -0400

    update

--- menu.ps1
+++ menu.ps1
@@ -2,12 +2,12 @@ clear
 $a=Read-Host -Prompt "
 `n1. Show day of the week
 `n2. Show year
-`n3
+`n3. Check for updates
 `nPlease select an option"
 switch ($a){
     1{Get-Date -UFormat %A}
     2{Get-Date -UFormat %Y}
-    3{}
+    3{Install-WindowsUpdate -AcceptAll -AutoReboot}
 }
 
 Write-Host $a
