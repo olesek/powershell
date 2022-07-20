e6ef48a4d9830a02ed3ce7027e0e1458c193c4a7
Joe Oleszczuk <joe@rattle.net>
2022-07-20 11:26:47 -0400

    update

---
+++ update-windows-pc.ps1
@@ -0,0 +1,9 @@
+# Update PC
+Install-PackageProvider -Name NuGet Force
+Install-Module -Name PSWindowsUpdate -Force
+Import-Module PSWindowsUpdate
+Get-Command -Module PSWindowsUpdate
+Get-WindowsUpdate
+Get-WURebootStatus
+Install-WindowsUpdate -AcceptAll -AutoReboot
+
