7f32deab5483fdf0adfd4069dc7ed3a2ee75def9
Joe Oleszczuk <joe@rattle.net>
2022-07-20 11:34:50 -0400

    update

--- update-windows-pc.ps1
+++ update-windows-pc.ps1
@@ -1,9 +1,11 @@
-# Update PC
+# Update PC - these commands only need to be run 1 time
 Install-PackageProvider -Name NuGet -Force
 Install-Module -Name PSWindowsUpdate -Force
 Import-Module PSWindowsUpdate
 Get-Command -Module PSWindowsUpdate
 Get-WindowsUpdate
 Get-WURebootStatus
+
+# Update PC - this command updates the PC
 Install-WindowsUpdate -AcceptAll -AutoReboot
 
