cc30027a635389d9087fc392c969179d23afc925
Joe Oleszczuk <joe@rattle.net>
2022-07-20 11:45:21 -0400

    update

---
+++ install-chrome.ps1
@@ -0,0 +1,5 @@
+write-host "Installing Chrome.."
+$installer_name = 'chrome'
+Invoke-WebRequest -uri 'http://dl.google.com/chrome/install/375.126/chrome_installer.exe' -OutFile "C:\windows\temp\$($installer_name).exe"
+Start-Process "C:\windows\temp\$($installer_name).exe" -ArgumentList "/silent /install" -Wait
+Remove-Item "C:\windows\temp\$($installer_name).exe" -Force
