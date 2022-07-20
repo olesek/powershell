749ca0ea337655c1c7c1fed6fe291b3cbc36a860
Joe Oleszczuk <joe@rattle.net>
2022-07-20 13:09:19 -0400

    update

--- install-chrome.ps1
+++ install-chrome.ps1
@@ -9,8 +9,10 @@ Set-Location c:\ps
 Invoke-WebRequest -uri 'http://dl.google.com/chrome/install/375.126/chrome_installer.exe' -OutFile "C:\ps\chrome_installer.exe"
 Start-Process "C:\ps\chrome_installer.exe" -ArgumentList "/silent /install" -Wait
 
+<#
 write-host "Installing Chrome.."
 $installer_name = 'chrome'
 Invoke-WebRequest -uri 'http://dl.google.com/chrome/install/375.126/chrome_installer.exe' -OutFile "C:\windows\temp\$($installer_name).exe"
 Start-Process "C:\windows\temp\$($installer_name).exe" -ArgumentList "/silent /install" -Wait
 Remove-Item "C:\windows\temp\$($installer_name).exe" -Force
+#>
