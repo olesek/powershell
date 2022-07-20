5c73fbe4195dbcbe96c5a17d7549d8c361d23220
Joe Oleszczuk <joe@rattle.net>
2022-07-20 11:59:35 -0400

    update

--- install-chrome.ps1
+++ install-chrome.ps1
@@ -1,6 +1,9 @@
 # Install Chrome
 # Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 
+Invoke-WebRequest -uri 'http://dl.google.com/chrome/install/375.126/chrome_installer.exe' -OutFile "C:\ps\chrome_installer.exe"
+Start-Process "C:\ps\chrome_installer.exe" -ArgumentList "/silent /install" -Wait
+
 write-host "Installing Chrome.."
 $installer_name = 'chrome'
 Invoke-WebRequest -uri 'http://dl.google.com/chrome/install/375.126/chrome_installer.exe' -OutFile "C:\windows\temp\$($installer_name).exe"
