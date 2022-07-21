8d013605465efedc4500060666d57aef97596a8d
Joe Oleszczuk <joe@rattle.net>
2022-07-21 14:39:35 -0400

    update

--- aob-pc-setup.ps1
+++ aob-pc-setup.ps1
@@ -1,4 +1,4 @@
-// Last modified: 2022/07/21 14:36:32
+// Last modified: 2022/07/21 14:39:28
 
 # This script originated from:
 # https://techexpert.tips/powershell/powershell-creating-user-menu/
@@ -76,7 +76,7 @@ function DisplayMenu {
     |    3) SET PC NAME & TIME ZONE                   |
     |    4) INSTALL GOOGLE CHROME                     |
     |    5) INSTALL GOOGLE CREDENTIAL PROVIDER FOR WINDOWS|
-    |    6)  DOGS & CATS                                    |
+    |    6)  DOGS & CATS & BEARS                           |
     |    7) UPGRADE THIS SCRIPT                 |
     |    8) EXIT                                      |
     +=================================================+
