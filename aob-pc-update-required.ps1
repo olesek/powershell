f3c5eb7f8a723bef79a8dec60373cde8a8280a2c
Joe Oleszczuk <joe@rattle.net>
2022-10-11 14:28:42 -0400

    update

--- aob-pc-update-required.ps1
+++ aob-pc-update-required.ps1
@@ -1,6 +1,25 @@
-$updates
+<#$updates
 
 Get-Command -Module PSWindowsUpdate
 if (PSWindowsUpdate -eq false) {
     "whops"
 }
+#>
+
+
+$updateInfoMsg = "Windows Update Status: `n";
+
+$UpdateSession = New-Object -ComObject Microsoft.Update.Session;
+$UpdateSearcher = $UpdateSession.CreateupdateSearcher();
+$Updates = @($UpdateSearcher.Search("IsAssigned=1 and IsHidden=0 and IsInstalled=0 and Type='Software'").Updates);
+$Found = ($Updates | Select-Object -Expand Title);
+
+If ($Found -eq $Null) {
+    $updateInfoMsg += "Up to date";
+} Else {
+    $Found = ($Updates | Select-Object -Expand Title) -Join "`n";
+    $updateInfoMsg += "Updates available:`n";
+    $updateInfoMsg += $Found;
+}
+
+$updateInfoMsg;
